bbf1612074cdeaca7604dc7543c3ba6eba09e81a
Sebastian David Estrada Ojeda <sdavide1010@gmail.com>
2023-08-23 12:35:23 -0500

    Fixed server routes

--- src/services/apiRoutes/index.ts
+++ src/services/apiRoutes/index.ts
@@ -1,4 +1,8 @@
-export const SERVER_ROUTE = process.env.BACKEND_ENDPOINT;
+export const SERVER_ROUTE =
+    process.env.BACKEND_ENDPOINT_DEV ||
+    process.env.BACKEND_ENDPOINT_STAGING ||
+    process.env.BACKEND_ENDPOINT_MAIN ||
+    process.env.BACKEND_ENDPOINT_PROD;
 
 export const API_ROUTES = {
     hotels: "/hotels",
